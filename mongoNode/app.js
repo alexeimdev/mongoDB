a1b3f00d22e1769d856f0dc2cf48a9641864be1c
alexeim <alexei.mel@gmail.com>
2017-03-22 22:10:11 +0200

    working simple connection to db

--- mongoNode/app.js
+++ mongoNode/app.js
@@ -1,3 +1,15 @@
-'use strict';
 
-console.log('Hello mongoDB Node!');
+var MongoClient = require('mongodb').MongoClient;
+
+var connectionString = "mongodb://localhost:27017";
+var dbName = "interactive_poll";
+var collectionName = "poll_questions";
+
+MongoClient.connect(connectionString + "/" + dbName, function (err, db) {
+    db.collection(collectionName, function (err, collection) {
+        collection.find().toArray(function (err, items) {
+            var result = JSON.stringify(items);
+            console.log(result);
+        });
+    });
+});
