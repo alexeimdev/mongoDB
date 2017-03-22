0a8e4e1e0d962d821d38b4d6dc8eba306086b0ec
alexeim <alexei.mel@gmail.com>
2017-03-22 21:19:46 +0200

    working simple connection to db

--- mongo.NET/Program.cs
+++ mongo.NET/Program.cs
@@ -1,12 +1,26 @@
-﻿
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using System;
 
 namespace mongo.NET
 {
     class Program
     {
+        private static string ConnectionString = "mongodb://localhost:27017";
+        private static string DbName = "interactive_poll";
+        private static string CollectionName = "poll_questions";
+
         static void Main(string[] args)
         {
-            
+            var mongoClient = new MongoClient(ConnectionString);
+            var db = mongoClient.GetDatabase(DbName);
+            var collection = db.GetCollection<BsonDocument>(CollectionName);
+
+
+            var result = collection.Find("{}");
+            Console.WriteLine(result.SingleOrDefault());
+            Console.ReadKey();
         }
+
     }
 }
