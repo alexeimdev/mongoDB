5afd2beb3759ba0fad52432f144ffce7ecea7496
alexeim <alexei.mel@gmail.com>
2017-03-26 20:40:39 +0300

    1. changed db 2. query type extended

--- mongo.NET/Program.cs
+++ mongo.NET/Program.cs
@@ -7,8 +7,11 @@ namespace mongo.NET
     class Program
     {
         private static string ConnectionString = "mongodb://localhost:27017";
-        private static string DbName = "interactive_poll";
-        private static string CollectionName = "poll_questions";
+        private static string DbName = "m101";
+        private static string CollectionName = "hw1";
+
+        enum QueryType { Linq, SqlLike, Native };
+
 
         static void Main(string[] args)
         {
@@ -16,11 +19,33 @@ namespace mongo.NET
             var db = mongoClient.GetDatabase(DbName);
             var collection = db.GetCollection<BsonDocument>(CollectionName);
 
+            var queryType = QueryType.Linq;
+            string strResult;
 
-            var result = collection.Find("{}");
-            Console.WriteLine(result.SingleOrDefault());
-            Console.ReadKey();
+            switch (queryType)
+            {
+                case QueryType.Linq:
+                    var resLinq= collection.Find(x => x.Equals("answer" == "42.0"));
+                    strResult = resLinq.SingleOrDefault().ToString();
+                    print(strResult);
+                    break;
+                case QueryType.SqlLike:
+                    break;
+                case QueryType.Native:
+                    var resNative = collection.Find("{}");
+                    strResult = resNative.SingleOrDefault().ToString();
+                    print(strResult);
+                    break;
+            }
+
+
         }
 
+        private static void print(string str)
+        {
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
     }
+
 }
