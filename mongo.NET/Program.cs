829ea0c4c7f058290fed0b7ec2ac29a3acff3d00
alexeim <alexei.mel@gmail.com>
2017-04-01 15:56:50 +0300

    1. added MainAsync 2. chenged db to "students" and  collection to "grades"

--- mongo.NET/Program.cs
+++ mongo.NET/Program.cs
@@ -1,50 +1,36 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Bson;
 using System;
+using System.Threading.Tasks;
 
 namespace mongo.NET
 {
     class Program
     {
         private static string ConnectionString = "mongodb://localhost:27017";
-        private static string DbName = "m101";
-        private static string CollectionName = "hw1";
-
-        enum QueryType { Linq, SqlLike, Native };
-
+        private static string DbName = "students";
+        private static string CollectionName = "grades";
 
         static void Main(string[] args)
         {
+            MainAsync(args).Wait();
+            Console.Read();
+
+        }
+
+        static async Task MainAsync(string[] args)
+        {
             var mongoClient = new MongoClient(ConnectionString);
+
             var db = mongoClient.GetDatabase(DbName);
-            var collection = db.GetCollection<BsonDocument>(CollectionName);
+            var col = db.GetCollection<BsonDocument>(CollectionName);
 
-            var queryType = QueryType.Linq;
-            string strResult;
-
-            switch (queryType)
-            {
-                case QueryType.Linq:
-                    var resLinq= collection.Find(x => x.Equals("answer" == "42.0"));
-                    strResult = resLinq.SingleOrDefault().ToString();
-                    print(strResult);
-                    break;
-                case QueryType.SqlLike:
-                    break;
-                case QueryType.Native:
-                    var resNative = collection.Find("{}");
-                    strResult = resNative.SingleOrDefault().ToString();
-                    print(strResult);
-                    break;
-            }
-
-
+            var res = await col.FindAsync("{}");
         }
 
         private static void print(string str)
         {
             Console.WriteLine(str);
-            Console.ReadKey();
         }
     }
 
